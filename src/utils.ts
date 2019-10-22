ea12a50cc185f37af6f71a4a393c6838a66b7c02
Jack Huang <w305jack@gmail.com>
2019-10-23 01:57:30 +0800

    adjust utils 2

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,10 @@
+require('dotenv').config();
+
 export function APIHost(): string {
   if (!!process.env.NODE_ENV) {
-    const dotenv = require('dotenv');
-    dotenv.config();
 
     console.log(process.env.HOST)
-    
+
     return process.env.HOST
 
   } else {
