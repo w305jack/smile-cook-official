c52dc11a2a7f679d348d4c52d3cf0e3a779321ca
Jack Huang <w305jack@gmail.com>
2019-10-22 16:42:37 +0800

    move server js

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var proxy = require('http-proxy-middleware');
+var path = require('path');
+var serveStatic = require('serve-static');
+
+app = express();
+app.use(serveStatic(__dirname + "/dist"));
+app.use(
+  '/api/',
+  proxy({ 
+    target: process.env.HOST, changeOrigin: true ,
+    pathRewrite: function (path, req) { return path.replace('/api/', process.env.HOST) }
+  })
+);
+
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
