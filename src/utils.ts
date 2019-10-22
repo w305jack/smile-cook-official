3e02153a61b9b9c377754371015c47d1aee8458c
Jack Huang <w305jack@gmail.com>
2019-10-22 23:33:28 +0800

    add test log 8

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,8 @@
 export function APIHost(): string {
   console.log('env:' + process.env.NODE_ENV)
   console.log('host:' + process.env.HOST)
-  
+  console.log('test:' + process.env.TEST)
+
   if (!!process.env.NODE_ENV) {
     return process.env.HOST
   } else {
