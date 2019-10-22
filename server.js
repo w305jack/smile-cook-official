a32a98f65c6eca70ce2f82461de762a315cb39c5
Jack Huang <w305jack@gmail.com>
2019-10-23 05:14:39 +0800

    use proxy

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ var cors = require('cors');
 app = express();
 app.use(cors());
 app.use(serveStatic(__dirname + "/dist"));
-// app.use(
-//   '/api/',
-//   proxy({ 
-//     target: process.env.HOST, changeOrigin: true ,
-//     pathRewrite: function (path, req) { return path.replace('/api/', process.env.HOST) }
-//   })
-// );
+app.use(
+  '/api/',
+  proxy({ 
+    target: process.env.HOST, changeOrigin: true ,
+    pathRewrite: function (path, req) { return path.replace('/api/', process.env.HOST) }
+  })
+);
 
 var port = process.env.PORT || 5000;
 app.listen(port);
