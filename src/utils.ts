1f5f5340d42711b0c532f694a0813014d0239b09
Jack Huang <w305jack@gmail.com>
2019-10-23 04:33:03 +0800

    adjust for package.json

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
+process.env.VUE_APP_API_URL = require('./package.json').apiUrl
+
 export function APIHost(): string {
   if (!!process.env.NODE_ENV) {
 
     console.log('vue:' + process.env.VUE_APP_SECRET)
 
-    console.log('url:' + process.env.API_URL)
+    console.log('url:' + process.env.VUE_APP_API_URL)
 
     return process.env.API_URL
 
