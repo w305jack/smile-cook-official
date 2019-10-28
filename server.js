57c0642e8267a97694796dda543bcc6e1d374d68
Jack Huang <w305jack@gmail.com>
2019-10-29 03:02:27 +0800

    final server.js

--- server.js
+++ server.js
@@ -1,26 +1,3 @@
-// var express = require('express');
-// var proxy = require('http-proxy-middleware');
-// var path = require('path');
-// var serveStatic = require('serve-static');
-// var cors = require('cors');
-// var history = require('connect-history-api-fallback');
-
-// app = express();
-// app.use(cors());
-// app.use(serveStatic(__dirname + "/dist"));
-// // app.use(
-// //   '/api/',
-// //   proxy({ 
-// //     target: process.env.API_URL, changeOrigin: true ,
-// //     pathRewrite: function (path, req) { return path.replace('/api/', process.env.API_URL) }
-// //   })
-// // );
-// app.use(history());
-
-// var port = process.env.PORT || 5000;
-// app.listen(port);
-// console.log('server started '+ port);
-
 const express = require('express');
 const path = require('path');
 const history = require('connect-history-api-fallback');
